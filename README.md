663a58d11d758c13fb7fe1285e22bb0585e1508e
cdsuddreth <cdsuddreth@gmail.com>
2020-05-14 20:33:14 -0400

    MVP advanced forms

--- README.md
+++ README.md
@@ -43,26 +43,45 @@ You can learn more in the [Create React App documentation](https://facebook.gith
 
 To learn React, check out the [React documentation](https://reactjs.org/).
 
-### Code Splitting
+Module Project: Advanced Form Management - User Onboarding
+Project Description
+We've seen many different styles of form management by now -- simple to complex. Today we are going to unleash your inner form-wizard! 🧙
 
-This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
+Set Up The Project
+[x] Start off by installing a blank React app by running npx create-react-app <app-name> --use-npm.
+[x] Using npm, add the following as dependencies inside your React app:
+    yup
+    axios
+[x] Create a component file called Form.js, import it into your App.js file, and place the component in your JSX there.
 
-### Analyzing the Bundle Size
+STEP 1 - Create Your Advanced Form
+We want to create a form to onboard a new user to our system. We need at least the following pieces of information about our new user:
+ Name
+ Email
+ Password
+ Terms of Service (checkbox)
+ A Submit button to send our form data to the server.
+STEP 2 - Implement Form Validation and Error Messaging
+Form validation is one of the facets of an application that makes it feel polished and controlled from a user perspective. With that in mind, implement the following:
 
-This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
+ Using Yup, set up at least two different validations along with custom error messages that will display on screen when validation fails.
+STEP 3 - Make a POST Request
+Being able to POST data is a key skill of any developer, no matter your skill level.
 
-### Making a Progressive Web App
+ Craft a POST request using axios that sends your form data to the following endpoint: https://reqres.in/api/users
+ Verify using a console.log() that you are receiving a successful response back
+(Note: For those that are curious, we're using reqres.in for this assignment's API. It's a free API that allows us to simulate a POST request for any data that we send it. Pretty awesome!)
 
-This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
+STEP 4 - Display Returned Data to Screen
+When you get your data back, you will want to do something with it, right? Let's display a list of users in our app.
 
-### Advanced Configuration
+ Set up a state property called users that is initialized with an empty array
+ Every time you make a POST request, and get that new user data back, update your users state with the new user added to the array
+ Render users in your app. You can use the html pre tag and JSON.stringify() method to display your post request.
+Stretch Goals
+The following are stretch goals that you should attempt after you meet MVP for your project:
 
-This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
-
-### Deployment
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
-
-### `npm run build` fails to minify
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+ Add basic styling to your form in your app. Make it look pretty with any styling method you choose.
+ Implement a dropdown menu in your form. Add a role value to your state and add a dropdown with different roles for your users.
+ Create 3 new inputs inside your form of your choice along with corresponding validation and error messaging
+ Add to your existing handling so that, if a user inputs their email as waffle@syrup.com, they receive an error message in their form that says "That email is already taken."
--- src/Form.js
+++ src/Form.js
@@ -1,74 +1,123 @@
 import React, { useState, useEffect } from "react";
 import * as yup from "yup";
-import axios from "axios";
+
 
 export default function Form(props) {
-        const [user, setUser] = useState({
+        const [formState, setFormState] = useState({
             name: "",
             email: "",
             password: "",
             terms: false
         });
     
-        const changeHandler = e => {
-            setUser({...user, [e.target.name]: e.target.value});
-            console.log(user);
-        };
-    
         const submitForm = e => {
             e.preventDefault();
-            props.addUser(user);
-            setUser({name:"", email:"", password:"", terms:false});
+            props.addUser(formState);
+            setFormState({name:"", email:"", password:"", terms:false});
         };
+
+        const formSchema = yup.object().shape({
+            name: yup.string().required("Name is a required field"),
+            email: yup
+              .string()
+              .email("Must be a valid email address")
+              .required("Must include email address"),
+            password: yup
+                .string()
+                .required("Must include a password")
+                .min(5)
+                .max(16),
+            terms: yup.boolean().oneOf([true], "Please agree to terms of use")
+          });
+
+        const [errorState, setErrorState] = useState({
+            name: "",
+            email: "",
+            motivation: "",
+            position: "",
+            terms: ""
+          });
         
+        const validate = e => {
+            let value = e.target.type === "checkbox" ? e.target.checked : e.target.value;
+            yup
+                .reach(formSchema, e.target.name)
+                .validate(value)
+                .then(valid => {
+                    setErrorState({
+                        ...errorState,
+                        [e.target.name]: ""
+                      });
+                })
+                .catch(err => {
+                    setErrorState({
+                      ...errorState,
+                      [e.target.name]: err.errors[0]
+                    });
+                  });
+        };
+
+        const inputChange = e => {
+            e.persist();
+            // console.log("input changed!", e.target.value, e.target.checked);
+            validate(e);
+            let value =
+              e.target.type === "checkbox" ? e.target.checked : e.target.value;
+            setFormState({ ...formState, [e.target.name]: value });
+          };
     
         return (
             <form className="form" onSubmit={submitForm}>
-                <label htmlFor="name">Name </label>
+                <label htmlFor="name">Name: </label>
                 <input 
-                    onChange={changeHandler}
+                    onChange={inputChange}
                     id="name"
                     type="text"
                     name="name"
                     placeholder="John Smith"
                     required
-                    value={user.name}
+                    value={formState.name}
                 />
                 <br/>
     
-                <label htmlFor="email">Email </label>
+                <label htmlFor="email">Email: </label>
                 <input 
-                    onChange={changeHandler}
+                    onChange={inputChange}
                     id="email"
                     type="text"
                     name="email"
                     placeholder="jsmith@example.com"
                     required
-                    value={user.email}
+                    value={formState.email}
                 />
                 <br/>
                 
-                <label htmlFor="password">Password </label>
+                <label htmlFor="password">Password: </label>
                 <input 
-                    onChange={changeHandler}
+                    onChange={inputChange}
                     id="password"
                     type="password"
                     name="password"
                     placeholder="password"
                     minlength="8"
                     required
-                    value={user.password}
+                    value={formState.password}
                 />
                 <br/>
 
-                <label htmlFor="terms">I have read and accept the Terms and Conditions.</label>
+                <label htmlFor="terms">I have read and accept the Terms and Conditions. </label>
                 <input 
-                    onChange={changeHandler}
+                    onChange={inputChange}
                     id="terms"
                     type="checkbox"
                 />
+
+                <br />
+
+                <div className="button-holder">
+                    <button type="submit">Submit</button>
+                </div>
                 
-                <button type="submit">Submit</button>
     
             </form>
     
